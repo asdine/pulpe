4544cb7949b4bcc3e5e10dc549549c133e08dbb9
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2017-02-05 11:32:17 +0100

    Mongodb create and fetch list

--- mongo/list.go
+++ mongo/list.go
@@ -1,9 +1,57 @@
 package mongo
 
-import "github.com/blankrobot/pulpe"
+import (
+	"time"
+
+	mgo "gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+
+	"github.com/blankrobot/pulpe"
+)
+
+const listCol = "lists"
 
 // Ensure ListService implements pulpe.ListService.
 var _ pulpe.ListService = new(ListService)
+
+// List representation stored in MongoDB.
+type List struct {
+	ID        bson.ObjectId `bson:"_id"`
+	PublicID  string        `bson:"publicID"`
+	CreatedAt time.Time     `bson:"createdAt"`
+	UpdatedAt *time.Time    `bson:"updatedAt,omitempty"`
+	BoardID   string        `bson:"boardID"`
+	Name      string        `bson:"name"`
+}
+
+// ToMongoList creates a mongo list from a pulpe list.
+func ToMongoList(p *pulpe.List) *List {
+	return &List{
+		ID:        bson.NewObjectId(),
+		PublicID:  string(p.ID),
+		CreatedAt: p.CreatedAt,
+		UpdatedAt: p.UpdatedAt,
+		BoardID:   string(p.BoardID),
+		Name:      p.Name,
+	}
+}
+
+// FromMongoList creates a pulpe list from a mongo list.
+func FromMongoList(l *List) *pulpe.List {
+	p := pulpe.List{
+		ID:        pulpe.ListID(l.PublicID),
+		CreatedAt: l.CreatedAt,
+		BoardID:   pulpe.BoardID(l.BoardID),
+		Name:      l.Name,
+	}
+
+	if l.UpdatedAt != nil {
+		t := (*l.UpdatedAt).UTC()
+		p.UpdatedAt = &t
+	}
+
+	return &p
+}
 
 // ListService represents a service for managing lists.
 type ListService struct {
@@ -12,12 +60,40 @@ type ListService struct {
 
 // CreateList creates a new List
 func (s *ListService) CreateList(l *pulpe.ListCreate) (*pulpe.List, error) {
-	return nil, nil
+	if l.BoardID == "" {
+		return nil, pulpe.ErrListBoardIDRequired
+	}
+
+	var err error
+	list := pulpe.List{
+		BoardID: l.BoardID,
+		Name:    l.Name,
+	}
+
+	list.ID, err = pulpe.NewListID()
+	if err != nil {
+		return nil, err
+	}
+
+	list.CreatedAt = s.session.now
+
+	return &list, s.session.db.C(listCol).Insert(ToMongoList(&list))
 }
 
 // List returns a List by ID.
 func (s *ListService) List(id pulpe.ListID) (*pulpe.List, error) {
-	return nil, nil
+	var l List
+
+	err := s.session.db.C(listCol).Find(bson.M{"publicID": string(id)}).One(&l)
+	if err != nil {
+		if err == mgo.ErrNotFound {
+			return nil, pulpe.ErrListNotFound
+		}
+
+		return nil, err
+	}
+
+	return FromMongoList(&l), nil
 }
 
 // DeleteList deletes a List by ID.
---
+++ mongo/list_test.go
@@ -0,0 +1,77 @@
+package mongo_test
+
+import (
+	"testing"
+
+	"github.com/blankrobot/pulpe"
+	"github.com/stretchr/testify/require"
+)
+
+func TestListService_CreateList(t *testing.T) {
+	session, cleanup := MustGetSession(t)
+	defer cleanup()
+
+	s := session.ListService()
+
+	t.Run("New", func(t *testing.T) {
+		l := pulpe.ListCreate{
+			BoardID: "BoardID",
+			Name:    "Name",
+		}
+
+		// Create new list.
+		list, err := s.CreateList(&l)
+		require.NoError(t, err)
+		require.NotZero(t, list.ID)
+		require.Equal(t, list.Name, l.Name)
+
+		// Retrieve list and compare.
+		other, err := s.List(list.ID)
+		require.NoError(t, err)
+		require.Equal(t, list.ID, other.ID)
+		require.Equal(t, list.Name, other.Name)
+		require.Equal(t, list.BoardID, other.BoardID)
+		require.Equal(t, list.CreatedAt.UTC(), other.CreatedAt.UTC())
+		require.Equal(t, list.UpdatedAt, other.UpdatedAt)
+	})
+
+	t.Run("No BoardID", func(t *testing.T) {
+		// Trying to create a card with no ID.
+		var l pulpe.ListCreate
+
+		_, err := s.CreateList(&l)
+		require.Equal(t, pulpe.ErrListBoardIDRequired, err)
+	})
+}
+
+func TestListService_List(t *testing.T) {
+	session, cleanup := MustGetSession(t)
+	defer cleanup()
+
+	s := session.ListService()
+
+	t.Run("Exists", func(t *testing.T) {
+		l := pulpe.ListCreate{
+			BoardID: "BoardID",
+		}
+
+		// Create new list.
+		list, err := s.CreateList(&l)
+		require.NoError(t, err)
+
+		// Retrieve list and compare.
+		other, err := s.List(list.ID)
+		require.NoError(t, err)
+		require.Equal(t, list.ID, other.ID)
+		require.Equal(t, list.Name, other.Name)
+		require.Equal(t, list.BoardID, other.BoardID)
+		require.Equal(t, list.CreatedAt.UTC(), other.CreatedAt.UTC())
+		require.Equal(t, list.UpdatedAt, other.UpdatedAt)
+	})
+
+	t.Run("Not found", func(t *testing.T) {
+		// Trying to fetch a list that doesn't exist.
+		_, err := s.List("QQQ")
+		require.Equal(t, pulpe.ErrListNotFound, err)
+	})
+}
