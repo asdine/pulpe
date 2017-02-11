c129fb2ff6e93fb4099ffe1622085c5a8d679671
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2017-02-11 18:29:27 +0100

    Production determinist caching

--- web/config/prod.js
+++ web/config/prod.js
@@ -1,6 +1,8 @@
 import webpack from 'webpack';
 import merge from 'webpack-merge';
 import ExtractTextPlugin from 'extract-text-webpack-plugin';
+import ChunkManifestPlugin from 'chunk-manifest-webpack-plugin';
+import WebpackChunkHash from 'webpack-chunk-hash';
 import baseConfig from './base';
 import packageConfig from '../package.json';
 
@@ -44,8 +46,23 @@ export default merge(baseConfig, {
 
   plugins: [
     new ExtractTextPlugin('style.css'),
+    // new webpack.optimize.CommonsChunkPlugin({
+    //   name: ['vendor', 'manifest'],
+    //   minChunks: Infinity,
+    // }),
+    // new ChunkManifestPlugin({
+    //   filename: 'chunk-manifest.json',
+    //   manifestVariable: 'webpackManifest'
+    // })
     new webpack.optimize.CommonsChunkPlugin({
-      name: ['vendor', 'manifest']
+      name: ['vendor', 'manifest'], // vendor libs + extracted manifest
+      minChunks: Infinity,
     }),
+    new webpack.HashedModuleIdsPlugin(),
+    new WebpackChunkHash(),
+    new ChunkManifestPlugin({
+      filename: 'chunk-manifest.json',
+      manifestVariable: 'webpackManifest'
+    })
   ]
 });
