ab263720ee72214dff058dc4613e9f758f09442f
jrmneveu <jdp.neveu@gmail.com>
2017-02-12 23:06:24 +0100

    Remove -xs for confirm modals

--- web/app/components/Modal.jsx
+++ web/app/components/Modal.jsx
@@ -58,7 +58,7 @@ export const ConfirmModal = ({ isOpen = false, onConfirm, text, toggle, delay })
   <Modal isOpen={isOpen} toggle={toggle}>
     <div className="modal-body">
       <div className="row">
-        <div className="col-xs-5 offset-xs-1">
+        <div className="col-5 offset-1">
           <button
             type="button"
             className="btn btn-danger btn-block"
@@ -70,7 +70,7 @@ export const ConfirmModal = ({ isOpen = false, onConfirm, text, toggle, delay })
             {text}
           </button>
         </div>
-        <div className="col-xs-5">
+        <div className="col-5">
           <button type="button" className="btn btn-secondary btn-block" onClick={toggle}>Cancel</button>
         </div>
       </div>
