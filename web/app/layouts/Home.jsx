9a04297b43b7bdcdab25e2c9677bb2c7eb0eb494
jrmneveu <jdp.neveu@gmail.com>
2017-02-26 15:23:49 +0100

    Add navbar for mobile with reactstrap

--- web/app/layouts/Home.jsx
+++ web/app/layouts/Home.jsx
@@ -1,3 +1,4 @@
+import { Collapse, Navbar, NavbarToggler, Nav, NavItem, NavLink } from 'reactstrap';
 import React from 'react';
 import BoardDetail from '../containers/board';
 import BoardList from '../containers/boardList';
@@ -18,9 +19,42 @@ const Menu = () =>
     <BoardList />
   </div>;
 
-const MobileMenu = () =>
-  <div className="plp-top-menu hidden-md-up">
-    <h1>pulpe</h1>
-  </div>;
+class MobileMenu extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggleNavbar = this.toggleNavbar.bind(this);
+    this.state = {
+      collapsed: true
+    };
+  }
+
+  toggleNavbar() {
+    this.setState({
+      collapsed: !this.state.collapsed
+    });
+  }
+  render() {
+    return (
+      <div className="plp-top-menu hidden-md-up">
+        {/* <h1>pulpe</h1>*/}
+        <Navbar light>
+          <NavbarToggler onClick={this.toggleNavbar} />
+          <h1>pulpe</h1>
+          <Collapse className="navbar-toggleable-md" isOpen={!this.state.collapsed}>
+            <Nav navbar>
+              <NavItem>
+                <NavLink href="/test/">Components</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">Github</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
 
 export default Home;
